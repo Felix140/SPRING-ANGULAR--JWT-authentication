281f5d446b75a41a9f7fea0d6a007d5212fd26f8
Felix Valdez <fdvaldez14@gmail.com>
2023-09-12 12:02:49 +0200

    Creazione progetto SPRINGBOOT

---
+++ authentication-back/src/test/java/com/auth/jwt/backend/AuthenticationBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.auth.jwt.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticationBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
