6eb3e398d01c04a57ccba7eb75b4041e83ce219c
Felix Valdez <fdvaldez14@gmail.com>
2023-09-12 16:02:47 +0200

    FE: creazione dei component (login-form, welcome-content, content)

--- authentication-front/src/app/app.module.ts
+++ authentication-front/src/app/app.module.ts
@@ -5,12 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { AuthContentComponent } from './auth-content/auth-content.component';
+import { WelcomeContentComponent } from './welcome-content/welcome-content.component';
+import { LoginContentComponent } from './login-content/login-content.component';
+import { ContentComponent } from './content/content.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    AuthContentComponent
+    AuthContentComponent,
+    WelcomeContentComponent,
+    LoginContentComponent,
+    ContentComponent
   ],
   imports: [
     BrowserModule,
