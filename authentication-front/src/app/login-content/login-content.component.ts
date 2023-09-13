645afb589f00f4923d9dc8c850474223e68a0a0b
Felix Valdez <fdvaldez14@gmail.com>
2023-09-13 13:22:00 +0200

    FE: Creazione dei Campi input e metodi di SUBMIT FORM

--- authentication-front/src/app/login-content/login-content.component.ts
+++ authentication-front/src/app/login-content/login-content.component.ts
@@ -11,14 +11,38 @@ export class LoginContentComponent {
   @Output() onSubmitLoginEvent = new EventEmitter();
   //* Avere nel componente padre(CONTENT) la richiesta di LOGIN data in (OUTPUT)-> mi permetterà di GESTIRE
   //* la risposta e portarla poi nella ->(AUTH-COMPONENT) una volta fatta l'autenticazione
+  @Output() onSubmitRegisterEvent = new EventEmitter();
 
   //? creo i campi input
   login: string = "";
   password: string = "";
 
+  active: string = 'login';
+  firstName: string = "";
+  lastName: string = "";
 
+  //? Questi metodi saranno utilizzati per switchare da un form all'altro
+  onLoginTab(): void {
+    this.active = 'login';
+  }
+  onRegisterTab(): void {
+    this.active = 'register';
+  }
+
+
+
+  //? Il metodo per quando clicco sul SUBMIT LOGIN
   onSubmitLogin(): void {
     this.onSubmitLoginEvent.emit({
+      login: this.login,
+      password: this.password
+    });
+  }
+  //? Il metodo per quando clicco sul SUBMIT REGISTER
+  onSubmitRegister(): void {
+    this.onSubmitRegisterEvent.emit({
+      firsName: this.firstName,
+      lastName: this.lastName,
       login: this.login,
       password: this.password
     });
