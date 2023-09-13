7e2dc7a5d6d15805c1f9679c999a2bcc4bc32808
Felix Valdez <fdvaldez14@gmail.com>
2023-09-13 13:27:33 +0200

    FE: creo richiesta per REGISTRAZIONE + endpoint AXIOS

--- authentication-front/src/app/content/content.component.ts
+++ authentication-front/src/app/content/content.component.ts
@@ -10,11 +10,23 @@ export class ContentComponent {
 
   constructor(private axiosService: AxiosService) { }
 
-  onLogin(input: any) {
+  onLogin(input: any): void {
     this.axiosService.request(
       "POST",
       "/login",
       {
+        login: input.login,
+        password: input.password
+      }
+    );
+  }
+  onRegister(input: any): void {
+    this.axiosService.request(
+      "POST",
+      "/register",
+      {
+        firsName: input.firsName,
+        lastName: input.lastName,
         login: input.login,
         password: input.password
       }
